08653fac4c39ec76483b2a9643eb9d15122bc329
HydrogenE7 <huangzhengdoc@163.com>
2026-07-04 23:10:29 +0800

    inside: update task.py + rlwe.py (author revision)

--- inside/deploy/src/rlwe.py
+++ inside/deploy/src/rlwe.py
@@ -1,5 +1,5 @@
 from sage.all import *
-from sigma import CurveHomomorphism, MaurerProof, q, O
+from sigma import CurveHomomorphism, MaurerProof, q, G, O
 
 
 R, x = ZZ["x"].objgen()
@@ -18,7 +18,7 @@ def rlwe_gen():
     k = ((a*s + e) % f - b) // qq
 
     assert (a*s + e) % f == (b + k*qq)
-    return (f2l(a), f2l(b)), (f2l(s), f2l(k), f2l(e))
+    return (f2l(a), f2l(b)), (f2l(s), f2l(e), f2l(k))
 
 
 class RLWEProof:
@@ -36,11 +36,12 @@ class RLWEProof:
     def encode(self, aux, crs):
         a, b = self.st
         
+        ### (sij)
         Y0, Gs0 = list(), list()
         Ax = [sum(a[j]*crs[(i+j)%n] for j in range(n)) for i in range(n)]
         Bx = sum(b[i]*crs[i] for i in range(n))
-        Ex = sum(sum(2**j*aux[2][i][j] for j in range(2))-1*crs[i] for i in range(n))
-        Kx = sum(sum(2**j*aux[1][i][j] for j in range(5))-16*crs[i] for i in range(n))
+        Ex = sum(sum(2**j*aux[1][i][j] for j in range(2))-1*crs[i] for i in range(n))
+        Kx = sum(aux[2][i] for i in range(n))
         Y0.extend([qq*Kx + Bx - Ex + 1*sum(Ax[i] for i in range(n))])
         Gs0.extend(
             [sum([[2**j*Ax[i] for j in range(2)] for i in range(n)], [])]
@@ -56,47 +57,43 @@ class RLWEProof:
             )
         phi0 = CurveHomomorphism(Gs0)
 
+        ### (eij)
         Y1, Gs1 = list(), list()
-        for j in range(5):
+        for j in range(2):
             Y1.extend([aux[1][i][j] for i in range(n)])
             Gs1.extend(
-                [[O]*i*5 + [O]*j + [crs[i]] + [O]*(5-j-1) + [O]*5*(n-i-1) for i in range(n)]
+                [[O]*i*2 + [O]*j + [crs[i]] + [O]*(2-j-1) + [O]*2*(n-i-1) for i in range(n)]
             )
             Y1.extend([aux[1][i][j] for i in range(n)])
             Gs1.extend(
-                [[O]*i*5 + [O]*j + [aux[1][i][j]] + [O]*(5-j-1) + [O]*5*(n-i-1) for i in range(n)]
+                [[O]*i*2 + [O]*j + [aux[1][i][j]] + [O]*(2-j-1) + [O]*2*(n-i-1) for i in range(n)]
             )
         phi1 = CurveHomomorphism(Gs1)
 
+        ### (ki)
         Y2, Gs2 = list(), list()
-        for j in range(2):
-            Y2.extend([aux[2][i][j] for i in range(n)])
-            Gs2.extend(
-                [[O]*i*2 + [O]*j + [crs[i]] + [O]*(2-j-1) + [O]*2*(n-i-1) for i in range(n)]
-            )
-            Y2.extend([aux[2][i][j] for i in range(n)])
-            Gs2.extend(
-                [[O]*i*2 + [O]*j + [aux[2][i][j]] + [O]*(2-j-1) + [O]*2*(n-i-1) for i in range(n)]
-            )
+        Y2.extend([aux[2][i] for i in range(n)])
+        Gs2.extend(
+            [[O]*i + [crs[i]] + [O]*(n-i-1) for i in range(n)]
+        )
         phi2 = CurveHomomorphism(Gs2)
 
         return (phi0, Y0), (phi1, Y1), (phi2, Y2)
 
     def prove(self, crs):
         assert self.wit is not None
-        s, k, e = self.wit
+        s, e, k = self.wit
         
         ss = [self.bitdecom(si+1, nbit=2) for si in s]
-        kk = [self.bitdecom(ki+16, nbit=5) for ki in k]
         ee = [self.bitdecom(ei+1, nbit=2) for ei in e]
 
         aux = [
             [[ss[i][j]*crs[i] for j in range(2)] for i in range(n)],
-            [[kk[i][j]*crs[i] for j in range(5)] for i in range(n)],
-            [[ee[i][j]*crs[i] for j in range(2)] for i in range(n)]
+            [[ee[i][j]*crs[i] for j in range(2)] for i in range(n)],
+            [k[i]*crs[i] for i in range(n)],
         ]
         sts = self.encode(aux, crs)
-        wits = [sum(ss, []), sum(kk, []), sum(ee, [])]
+        wits = [sum(ss, []), sum(ee, []), k]
         proof = [
             MaurerProof(st=st, wit=wit).prove()
             for st, wit in zip(sts, wits)
--- inside/deploy/src/task.py
+++ inside/deploy/src/task.py
@@ -26,7 +26,7 @@ MENU = '''==========
 |  [Q]uit
 =========='''
 
-crs, rlwe = None, None
+crs, st = None, None
 
 while True:
     print(MENU)
@@ -36,23 +36,17 @@ while True:
             raise Exception("ok~")
 
         elif inp == "c":
-            if crs is None:
-                tau = int(os.urandom(32).hex(), 16) % q
-                crs = [pow(tau, i, q)*G for i in range(n)]
-                del tau
-            else:
-                alpha = int(os.urandom(32).hex(), 16) % q
-                crs = [pow(alpha, i, q)*crs[i] for i in range(n)]
-                del alpha
+            tau = int(os.urandom(32).hex(), 16) % q
+            crs = [pow(tau, i, q)*G for i in range(n)]
+            del tau
             print(f"crs = {list(map(point2tuple, crs))}")
 
         elif inp == "r":
             st, _ = rlwe_gen()
-            rlwe = RLWEProof(st)
-            print(f"rlwe = {rlwe}")
+            print(f"st = {st}")
 
         elif inp == "p":
-            if crs is None or rlwe is None:
+            if crs is None or st is None:
                 print("nah...")
                 continue
             aux = ast.literal_eval(input("aux>"))
@@ -60,13 +54,14 @@ while True:
 
             aux = [
                 [[E(aux[0][i][j]) for j in range(2)] for i in range(n)],
-                [[E(aux[1][i][j]) for j in range(5)] for i in range(n)],
-                [[E(aux[2][i][j]) for j in range(2)] for i in range(n)]
+                [[E(aux[1][i][j]) for j in range(2)] for i in range(n)],
+                [E(aux[2][i]) for i in range(n)],
             ]
             proof = [
                 (list(map(tuple2point, R)), z) for R, z in proof
             ]
-            if rlwe.verify(crs, aux, proof):
+
+            if RLWEProof(st).verify(crs, aux, proof):
                 print("Congratulations!")
                 print(f"{os.environ.get('FLAG')}")
                 print("Hope you enjoy!")
